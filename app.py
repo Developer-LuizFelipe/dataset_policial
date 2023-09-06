16e9c418135845ec77f26921e45edb9d34dea8b6
Luiz Felipe <58983157+Developer-LuizFelipe@users.noreply.github.com>
2023-09-05 21:17:04 -0300

    commit

---
+++ app.py
@@ -0,0 +1,120 @@
+# pip install streamlit pandas matplotlib seaborn
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+st.set_page_config(page_title='Análise  das Ocorencias na cidade do rio de Janeiro ', layout='wide')
+
+df = pd.read_csv('BO_2013_1.CSV')
+
+# Metricas 
+df_final=df[['NOME_DEPARTAMENTO','ANO','MES','RUBRICA','CONDUTA','SEXO_PESSOA','COR','DESCR_GRAU_INSTRUCAO']]
+df_final=df_final.dropna()
+df_final=df_final.drop_duplicates()
+df_final=df_final.reset_index(drop=True)
+
+# Criando os filtros
+st.sidebar.title('Filtros')
+st.sidebar.markdown('Escolha os filtros para visualizar os gráficos.')
+
+# Criando os filtros
+ano = st.sidebar.multiselect(
+    "Selecione o ano:",
+    options=df_final['ANO'].unique(),
+    default=df_final['ANO'].unique()
+)
+
+mes = st.sidebar.multiselect(
+    "Selecione o mês:",
+    options=df_final['MES'].unique(),
+    default=df_final['MES'].unique()
+)
+rubrica = st.sidebar.multiselect(
+    "Selecione a rubrica:",
+     options=df_final['RUBRICA'].unique(),
+     default=df_final['RUBRICA'].unique()
+)
+
+conduta = st.sidebar.multiselect(
+    "Selecione a conduta:",
+    options=df_final['CONDUTA'].unique(),
+    default=df_final['CONDUTA'].unique()    
+)
+
+sexo = st.sidebar.multiselect(
+    "Selecione o sexo:",
+    options=df_final['SEXO_PESSOA'].unique(),
+    default=df_final['SEXO_PESSOA'].unique()
+)
+cor = st.sidebar.multiselect(
+    "Selecione a cor:",
+    options=df_final['COR'].unique(),
+    default=df_final['COR'].unique()
+)
+
+grau = st.sidebar.multiselect(
+    "Selecione o grau de instrução:",
+    options=df_final['DESCR_GRAU_INSTRUCAO'].unique(),
+    default=df_final['DESCR_GRAU_INSTRUCAO'].unique()
+)
+
+departamento = st.sidebar.multiselect(
+    "Selecione o departamento:",
+    options=df_final['NOME_DEPARTAMENTO'].unique(),
+    default=df_final['NOME_DEPARTAMENTO'].unique()
+)
+
+
+
+df_selection = df_final.query(
+    "ANO == @ano & MES == @mes & RUBRICA == @rubrica & CONDUTA == @conduta & SEXO_PESSOA == @sexo & COR == @cor & DESCR_GRAU_INSTRUCAO == @grau & NOME_DEPARTAMENTO == @departamento"
+)
+
+
+
+
+# Abas 
+
+# ABA 01 - Graficos 
+
+st.dataframe(df_selection)
+fig, ax = plt.subplots(figsize=(10, 5))
+sns.countplot(x='RUBRICA', data=df_selection)
+plt.xticks(rotation=90)
+st.pyplot(fig)
+
+
+#ABA 02 - MEDIA
+
+
+
+#ABA 03 - Descrição  
+
+
+
+
+
+
+
+# # criar grafico de barras
+# st.header('Gráfico de barras empilhadas')
+
+
+# # criar grafico de barras empilhadas
+# fig, ax = plt.subplots(figsize=(10, 5))
+# sns.countplot(x='MES', hue='RUBRICA', data=df_final)
+# st.pyplot(fig)
+
+# # criar grafico de barras
+# st.header('Gráfico de barras horizontais')
+
+
+# # criar grafico de barras horizontais
+# fig, ax = plt.subplots(figsize=(10, 5))
+# sns.countplot(y='RUBRICA', data=df_final)
+# plt.xticks(rotation=90)
+# st.pyplot(fig)
+
+
